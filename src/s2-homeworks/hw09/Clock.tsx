cb27f26982cc9cd5b5b0fc8d14f572295ff0a6b4
hadsome-Droid <xkumbhax@gmail.com>
2024-02-19 13:25:12 +0300

    homeWork 9 complete v2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -30,7 +30,7 @@ function Clock() {
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
         setShow(false)
     }
-    const time = date.toLocaleTimeString()
+    const time = date.toLocaleTimeString('en-GB', {hour12: true})
     const myDate = date.toLocaleDateString('en-GB')
 
     const stringTime = `${time}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
