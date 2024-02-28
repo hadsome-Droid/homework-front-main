aea6eb77aaccf9c1b9e5463f50cc2cad0471024a
hadsome-Droid <xkumbhax@gmail.com>
2024-02-28 20:05:25 +0300

    homework 13 complete v3

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -38,32 +38,33 @@ const HW13 = () => {
                 setImage(success200)
                 // дописать
                 setText(res.data.errorText)
+                setInfo(res.data.info)
+
             })
             .catch((e) => {
                 // дописать
-                if(e.response.status === 400){
+                if (e.response.status === 400) {
 
                     setCode('Код 400!')
                     setImage(error400)
                     setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
 
-                } else if (e.response.status  === 500) {
+                } else if (e.response.status === 500) {
 
                     setCode('Код 500!')
                     setImage(error500)
                     setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
 
-                } else{
+                } else {
 
-                    setCode('Неизвестный код!')
+                    setCode('Error')
                     setImage(errorUnknown)
-                    setText('Error')
-                    console.log(e)
+                    setText(e.message)
+                    setInfo(e.name)
                 }
 
-            })
-            .finally(() => {
-                setInfo('')
             })
     }
 
